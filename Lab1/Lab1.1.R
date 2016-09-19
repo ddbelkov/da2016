8cc4d99aa406610007e11ff3d8a446351841a362
Daniil <d.d.belkov@gmail.com>
2016-09-19 12:39:32 +0500

    Lab 1 is done

---
+++ Lab1/Lab1.1.R
@@ -0,0 +1,6 @@
+m <-runif(400)
+m <-matrix(m, nrow = 20)
+rownames(m)<-letters[1:20]
+heatmap(m)
+#Plot(m)
+#hist(m)
---
+++ Lab1/Lab1.2.R
@@ -0,0 +1,8 @@
+library(ggplot2)
+qplot(data = iris, x = Sepal.Length, y = Petal.Length)
+qplot(data = iris,
+      x = Sepal.Length, 
+      y = Petal.Length,
+      color = Species,
+      size  = Petal.Width,
+      alpha = I(0.7))
